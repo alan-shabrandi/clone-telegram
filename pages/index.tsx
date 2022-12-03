bc1e67b7d5faf3d127a857a8447af89da6f8caeb
alan.shabrandi <alan.shabrandi@gmail.com>
2022-12-03 16:58:29 +0330

    main layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import MainLayout from '../src/components/layouts/mainLayout.index'
 
 export default function Home() {
   return (
-    <div></div>
+    <div>
+      <MainLayout />
+    </div>
   )
 }
